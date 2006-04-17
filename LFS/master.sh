c2c8b1fd4d8deedb8af2cffda77286a6fe932546
Manuel Canales Esparcia <macana@macana-es.com>
2006-04-17 23:05:08 +0000

    Simplified Readline reinstallation.

--- LFS/master.sh
+++ LFS/master.sh
@@ -161,13 +161,10 @@ chapter6_Makefiles() {
     done
     # Remove Bzip2 binaries before make install
     sed -e 's@make install@rm -vf /usr/bin/bz*\n&@' -i chapter06$N/*-bzip2
-    # Tell Module-Init-Tools that we are reinstalling it
+    # Fix how Module-Init-Tools do the install target
     sed -e 's@make install@make INSTALL=install install@' -i chapter06$N/*-module-init-tools
-    # Delete *old Readline libraries and regenerate two links after make install
-    sed -e 's@make install@&\nln -sfv libreadline.so.5.1 /lib/libreadline.so.5@' \
-        -e 's@make install@&\nln -sfv libhistory.so.5.1 /lib/libhistory.so.5@' \
-        -e 's@make install@&\nrm -v /lib/lib{history,readline}*old@' \
-        -e 's@make install@&\nrm -v /lib/lib{history,readline}.so.5@' -i chapter06$N/*-readline
+    # Delete *old Readline libraries just after make install
+    sed -e 's@make install@&\nrm -v /lib/lib{history,readline}*old@' -i chapter06$N/*-readline
     # Let some Udev pre-installation commands to fail
     sed -e 's@/lib/udev/devices/fd@& || true@' \
         -e 's/mknod -m.*/& || true/' -i chapter06$N/*-udev
