d1d8eb54944e20b33dc65dfef871bf1e2ae75de0
Manuel Canales Esparcia <macana@macana-es.com>
2006-05-26 23:53:13 +0000

    Improved packages.sh. Started support for GNOME and KDE meta-packages.

--- BLFS/packages.sh
+++ BLFS/packages.sh
@@ -4,14 +4,53 @@
 #
 set -e
 
+#---------------------
+# packages module
+source func_packages
+[[ $? > 0 ]] && echo -e "\n\tERROR: func_packages did not load..\n" && exit
+
+
+if [[ -z "$1" ]] ; then
+  echo -e "n\tYou must to provide the name of the BLFS book sources directory.\n"
+  exit 1
+fi
+
 BLFS_XML=$1
 
-> packages.tmp
+if [[ ! -d $BLFS_XML ]] ; then
+  echo -e "\n\t$BLFS_XML is not a directory\n"
+  exit 1
+fi
 
-for file in `find $BLFS_XML -name "*.xml"` ; do
-  pkg_id=`grep "sect1 id" $file | sed -e 's/<sect1 id="//;s/".*//'`
-  [[ ! -z "$pkg_id" ]] && echo -e "$pkg_id\t$file" >> packages.tmp
-done
+if [[ ! -f $BLFS_XML/use-unzip.xml ]] ; then
+  echo -e "\n\tLooks like $BLFS_XML is not a BLFS book sources directory\n"
+  exit 1
+fi
 
-sort packages.tmp -o packages
-rm packages.tmp
+if [[ -n "$2" ]] ; then
+  case $2 in
+    update )
+      if [[ -d $BLFS_XML/.svn ]] ; then
+      echo -e "\n\tUpdating the $BLFS_XML book sources ...\n"
+        pushd $BLFS_XML 1> /dev/null
+        svn up
+        popd 1> /dev/null
+        echo -e "\n\tBook sources updated."
+      else
+        echo -e "\n\tLooks like $BLFS_XML is not a svn working copy."
+        echo -e "\tSkipping BLFS sources update.\n"
+      fi
+      ;;
+    * )
+      echo -e "\n\tUnknown option $2 ignored.\n"
+      ;;
+  esac
+fi
+
+echo -en "\n\tGenerating packages file ..."
+generate_packages
+echo "done."
+
+echo -en "\tGenerating gnome-core dependencies list ..."
+generate_gnome_core
+echo "done."
