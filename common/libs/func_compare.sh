b517696a7e83d6682c2e7b3c2742fa6f8460c464
Manuel Canales Esparcia <macana@macana-es.com>
2007-03-03 11:58:40 +0000

    Removed unmaintained PACO patch.

--- common/libs/func_compare.sh
+++ common/libs/func_compare.sh
@@ -55,7 +55,7 @@ wrt_compare_work() {               #
   local ITERATION=$1
   local   PREV_IT=$2
   local PRUNEPATH="/dev /home /${SCRIPT_ROOT} /lost+found /media /mnt /opt /proc \
-/sources /root /srv /sys /tmp /tools /usr/local /usr/src /var/log/paco"
+/sources /root /srv /sys /tmp /tools /usr/local /usr/src"
 
   local    ROOT_DIR=/
   local DEST_TOPDIR=/${SCRIPT_ROOT}
