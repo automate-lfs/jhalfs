211057204a51445835dd38c7ed56dfad5aa5293b
Manuel Canales Esparcia <macana@macana-es.com>
2007-09-14 16:47:17 +0000

    Changed grep version check. Thanks to Dan Nicholson for the fix.

--- common/libs/func_check_version.sh
+++ common/libs/func_check_version.sh
@@ -93,7 +93,7 @@ check_prerequisites() {      #
   check_version "2.8"     "$(diff --version  | head -n1 | cut -d" " -f4)"      "DIFF"
   check_version "4.1.20"  "$(find --version  | head -n1 | cut -d" " -f4)"      "FIND"
   check_version "3.0"     "$(gawk --version  | head -n1 | cut -d" " -f3)"      "GAWK"
-  check_version "2.5"     "$(grep --version  | head -n1 | cut -d" " -f4)"      "GREP"
+  check_version "2.5"     "$(grep --version  | head -n1 | awk '{print $NF}')"  "GREP"
   check_version "1.2.4"   "$(gzip --version 2>&1 | head -n1 | cut -d" " -f2)"  "GZIP"
   check_version "3.79.1"  "$(make --version  | head -n1 | cut -d " " -f3 | cut -c1-4)"  "MAKE"
   check_version "2.5.4"   "$(patch --version | head -n1 | cut -d" " -f2)"      "PATCH"
